d2d1e0a5e3deaa72d9767b6cad967fd9cc1b6112
dimasx1234 <45216468+dimasx1234@users.noreply.github.com>
2025-10-21 22:32:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // replace with your GitHub repo name
+  base: "/weathercow", // replace with your GitHub repo name
 });
