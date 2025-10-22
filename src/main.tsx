2424f0ca469deaee8daa70ab8a4f372d9573a34e
dimasx1234 <45216468+dimasx1234@users.noreply.github.com>
2025-10-22 21:56:30 +0200

    Comment out WeatherCalendar import in main.tsx
    Comment out import statement for WeatherCalendar component

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './WeatherTime.tsx'
-import App2 from './WeatherCalendar.tsx'
+//import App2 from './WeatherCalendar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
